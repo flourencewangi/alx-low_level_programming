a20a3f44aa379c6f821ac4682cf9e877d3409dee
flourencewangi <flourencewangi@gmail.com>
2022-09-08 10:54:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Return: Always 0 (success)
+ *
+ *    */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
