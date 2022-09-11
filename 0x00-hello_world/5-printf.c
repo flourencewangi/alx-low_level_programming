cfe0c3f144f8b38550f14be58a26e2a27c3da984
flourencewangi <flourencewangi@gmail.com>
2022-09-11 11:51:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   * Return: Always 0 (success)
- *
- *    */
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 
