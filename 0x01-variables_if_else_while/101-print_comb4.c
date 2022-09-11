7cfb014c950a3ad9c97d9b843cbc85e36dad91bc
flourencewangi <flourencewangi@gmail.com>
2022-09-11 11:17:58 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print possible combo of 3
+ *
+ *  Return: returns zero at the end
+ */
+int main(void)
+{
+	int i, p, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (p = 1; p <= 9; p++)
+		{
+			for (m = 2; m <= 9; m++)
+			{
+				if (m > p && p > i)
+				{
+					putchar(i + '0');
+					putchar(p + '0');
+					putchar(m + '0');
+						if (i != 7)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
