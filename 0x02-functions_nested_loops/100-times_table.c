338baa71ef7e4e20e2065373d5af330ca1e5f566
flourencewangi <flourencewangi@gmail.com>
2022-09-14 06:35:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,35 +7,35 @@
  */
 void print_times_table(int n)
 {
-	int digit, mult, result;
+int digit, mult, result;
 
-	if (n <= 15 && n >= 0)
-	{
-	for (digit = 0; digit <= n; digit++)
-	{
-	_putchar('0');
+if (n <= 15 && n >= 0)
+{
+for (digit = 0; digit <= n; digit++)
+{
+_putchar('0');
 
-	for (mult = 1; mult <= n; mult++)
-	{
-	_putchar(',');
-	result = digit * mult;
-	if (result <= 99)
-	_putchar(' ');
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+result = digit * mult;
+if (result <= 99)
+_putchar(' ');
 
-	if (result <= 9)
-	_putchar(' ');
-	if (result >= 100)
-	{
-	_putchar((result / 100) + '0');
-	_putchar((result / 10) % 10 + '0');
-	}
-	else if (result <= 99 && result >= 10)
-	{
-	_putchar((result / 10) + '0');
-	}
-	_putchar((result % 10) + '0');
-	}
-	_putchar('\n');
-	}
-	}
-	}	
+if (result <= 9)
+_putchar(' ');
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
