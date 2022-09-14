3b9a16b2830647f48e906e791f6772a758a346c6
flourencewangi <flourencewangi@gmail.com>
2022-09-14 04:53:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
-/*
+/**
  * main - print putchar
  * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -14,6 +14,8 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
+
 }
