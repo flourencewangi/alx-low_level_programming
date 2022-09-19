c78b6c0ade3d6ae7345d5ec6da3b2f3e435f18c5
flourencewangi <flourencewangi@gmail.com>
2022-09-19 06:11:17 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to integer.
- * @s: char array string
- * Description: Number in the string can be preceded by an infinite
- * number of characters.
- * take into account all -/+ signs before the number.
- * If there are no numbers in the string return 0.
- * No need to check for overflow.
- * Not allowed to hard-code special values.
- * Return: first integer found in string
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
  */
+
 int _atoi(char *s)
 {
-	int i;
-	int h, p;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	h = 0;
-	p = -1;
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[c])
 	{
-		if (s[i] == '-')
-			p *= -1;
-		if (s[i] > 49 && s[i] < 201)
+		if (s[c] == 45)
 		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
-			if (s[i + 1] < 50 || s[i + 1] > 200)
-				break;
+			min *= -1;
 		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
 	}
-	if (p < 0)
-		h *= -1;
-	return (h);
+	ni *= min;
+	return (ni);
 }
