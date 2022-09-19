8fa97c8bdb56325c1dcc2b31ccac3d154d3b473f
flourencewangi <flourencewangi@gmail.com>
2022-09-19 04:16:01 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,21 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * main - calls other functions
- *
- * Return: 0
+ * void prototypes()
+ * int prototypes()
  */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
