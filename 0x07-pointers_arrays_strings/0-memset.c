14af25b9334dc15559c38c40dfb9f3cc1522e2c7
flourencewangi <flourencewangi@gmail.com>
2022-09-26 04:28:52 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,26 +1,22 @@
-/**
- *  File: 0-memset.c
- *   Auth: not
- */
 #include "main.h"
+#include <string.h>
 
 /**
- * _memset - Fills the first n bytes of the memory area
- *   pointed to by @s with the constant byte @c.
- *   @s: A pointer to the memory area to be filled.
- *   @c: The character to fill the memory area with.
- *   @n: The number of bytes to be filled.
- *    description _memset: over there
- *
- *    Return: A pointer to the filled memory area @s.
- *
+ * _memset - fills memory with a constant byte
+ * @s: the memory space to be filled
+ * @b: a constant byte
+ * @n: number of bytes in the memory
+ * Return: a pointer to s
  */
-void *_memset(void *s, int c, size_t n)
-{
-	unsigned int index;
-	unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-	return (memory);
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+			return (s);
 }
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,22 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main.h 
- *  Auth: sam tech (samuel birhanu)
- *   description: Header file containing prototypes for all functions 
- *  used in the 0x06-pointers_arrays_strings directory.
- */
 
-#include <stddef.h>
+#include <stdio.h>
+
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);                                    
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);                                            void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);                                                                                          
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
 #endif
 
 
