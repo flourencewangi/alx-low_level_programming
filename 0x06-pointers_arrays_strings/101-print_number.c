b4b1a61bacee66250594bd800e43c6e02cc54e1d
flourencewangi <flourencewangi@gmail.com>
2022-09-26 10:57:06 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,26 +8,19 @@
 
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		n1 = -n;
 	}
-	else
+
+	if (n1 / 10 != 0)
 	{
-		m = n;
+		print_number(n1 / 10);
 	}
-	d = m;
-	count = 1;
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((n1 % 10) + '0');
 }
