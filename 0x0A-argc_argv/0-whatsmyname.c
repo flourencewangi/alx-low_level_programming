8f5c51e200ca51092af922f6fc73878a8e3e674a
flourencewangi <flourencewangi@gmail.com>
2022-10-02 10:40:01 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,10 @@
  *
  *Return: Always 0 (Success)
  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%s\n", argv[0]); 
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
