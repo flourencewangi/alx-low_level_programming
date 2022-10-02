b759fea19f2e786d8d559a9d78273c468a68d063
flourencewangi <flourencewangi@gmail.com>
2022-10-02 10:37:10 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
 
 /**
  * main - print the name of the program
- * @argc: Count arguments 
- * @argv: Arguments 
+ *@argc: Count arguments
+ *@argv: Arguments
  *
  * Return: Always 0 (Success)
  */
