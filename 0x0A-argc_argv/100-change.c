aca826086df3c4a4c91ddce1d82c37178e361c09
flourencewangi <flourencewangi@gmail.com>
2022-10-02 11:16:53 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	position = total = change = aux = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]);
+
+	if (total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (coins[position] != '\0')
+
+	{
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+
+		position++;
+	}
+	printf("%d\n", change);
+	return (0);
+}
