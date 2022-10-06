ef3fad6ccf70b2f416c3398283d0d4528522bfd0
flourencewangi <flourencewangi@gmail.com>
2022-10-06 06:54:22 -0700

    0-malloc_checked.c

--- 0-malloc_checked.c
+++ 0-malloc_checked.c
@@ -1,17 +1,41 @@
-#include <stdlib.h>
 #include "main.h"
-/**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: a pointer to the allocated memory
- */
-void *malloc_checked(unsigned int b)
-{
-	void *ptr;
+#include <stdlib.h>
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+/**
+ * string_nconcat - Concatenates two strings using at
+ *  most an inputted number of bytes.
+ *  @s1: The first string.
+ *  @s2: The second string.
+ *  @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ *  Return: If the function fails - NULL.
+ *  Otherwise - a pointer to the concatenated space in memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int len = n, index;
+
+	if (s1 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
--- main.h
+++ main.h
@@ -1,15 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdlib.h>
 
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
+/**
+ * malloc_checked - Allocates memoery using malloc
+ * @b:The number of bytes to be allocated
+ *
+ * Return:A pointer to the allocated memory
+ */
+void * malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
 
+	if(mem == NULL)
+		exit(98);
 
-#endif
+	return (mem);
+}
